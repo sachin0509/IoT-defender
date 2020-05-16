16d1ce97448086cefd29facb6807e6abbeef5822
sachin0509 <55988166+sachin0509@users.noreply.github.com>
2020-05-16 11:40:05 +0530

    Add files via upload

---
+++ prt.py
@@ -0,0 +1,48 @@
+import socket
+import logging
+import time
+
+
+class SConnect:
+
+    def __init__(self, ip, port=None):
+        self.ip = ip
+        self.port = port
+        self.address = (self.ip, self.port)
+        self.s_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s_connection.settimeout(0.3)
+
+    def portscan(self):
+        return self.s_connection.connect_ex(self.address)
+
+
+def main():
+    logging.basicConfig(filename="errlog.log", format="%(asctime)s : %(message)s")
+    logging.info("Start")
+    print("\nHello user and welcome to Network Port Scanner!")
+    print("Please insert a IP address that you want to scan for open and closed ports.")
+    print("The range of ports scanned is 1-65535.")
+    u_ip = input("\nTarget IP: ")
+    port = [22, 80, 23, 5060, 8080, 7547, 2323, 81, 25, 2222, 8081, 9200, 8090, 52869, 37777, 37215, 2332, 2223, 5061,
+            100]
+    open_pcounter = 0
+    closed_pcounter = 0
+
+    if u_ip is not None:
+        for p in port:
+            start_ptime = time.time()
+            c = SConnect(u_ip, p)
+            if c.portscan() == 0:
+                print("Port {} is open".format(p))
+                open_pcounter += 1
+    else:
+        print("You failed, terminating.\n")
+
+    print("Total open ports:", open_pcounter)
+    logging.info("Finished")
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
