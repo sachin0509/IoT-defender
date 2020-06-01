8501d8fec8eb232426e0366a7c8421d2b925dc31
sachin0509 <55988166+sachin0509@users.noreply.github.com>
2020-06-01 23:23:24 +0530

    Update prt.py

--- prt.py
+++ prt.py
@@ -1,8 +1,11 @@
 import socket
 import logging
 import time
+from tabulate import tabulate
+
 
 class SConnect:
+
     def __init__(self, ip, port=None):
         self.ip = ip
         self.port = port
@@ -17,31 +20,33 @@ class SConnect:
 def main():
     logging.basicConfig(filename="errlog.log", format="%(asctime)s : %(message)s")
     logging.info("Start")
-    print("\nHello user and welcome to Network Port Scanner!")
-    print("Please insert a IP address that you want to scan for open and closed ports.")
-    print("The range of ports scanned is 1-65535.")
+
     port = [22, 80, 23, 5060, 8080, 7547, 2323, 81, 25, 2222, 8081, 9200, 8090, 52869, 37777, 37215, 2332, 2223, 5061,
             100]
+    n = (
+        "22 \n80 \n23 \n5060 \n8080 \n7547 \n2323 \n81 \n25 \n2222 \n8081 \n9200 \n8090 \n52869 \n37777 \n37215 \n2332 \n2223 \n5061 \n100")
     open_pcounter = 0
+
     closed_pcounter = 0
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,)
     s.connect(("10.255.255.255", 80))
-    userIP=s.getsockname()[0]
-    print(userIP)
+    v_ip = s.getsockname()[0]
 
 
-    if userIP is not None:
+    print(tabulate([['Sys name',(socket.gethostbyaddr(socket.gethostname())[0])], ['Sys ip', v_ip]], headers=['\nSystem details', '              ']))
+    print(tabulate([['\nport numbers',  n]], headers=['\nList of ports being scanned', '                           ']))
+
+    if v_ip is not None:
         for p in port:
             start_ptime = time.time()
-            c = SConnect(userIP, p)
+            c = SConnect(v_ip, p)
             if c.portscan() == 0:
-                print("Port {} is open".format(p))
+                print(tabulate([['\nport number', p]], headers=['\nList of ports open', '                  ']))
                 open_pcounter += 1
     else:
         print("You failed, terminating.\n")
-
-    print("Total open ports:", open_pcounter)
+    print(tabulate([['\nnumber of open ports', open_pcounter]],
+                   headers=['\nTotal number of ports open', '                          ']))
     logging.info("Finished")
 
 
