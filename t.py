2121234a5540cb74a30124d5bc032325e461cd32
cross111 <56246104+cross111@users.noreply.github.com>
2020-06-03 22:15:12 +0530

    Update t.py

--- t.py
+++ t.py
@@ -1,16 +1,16 @@
-import os
+from prettytable import PrettyTable
+table=PrettyTable()
+table.title="Data Encryption"
 def dataenc():
     f = os.popen('blkid /dev/sda1')
     n = f.read
-    flag=0
+    flag = 0
     for s in f:
         for word in s.split():
-            if word=='TYPE="crypto_LUKS"':
-                print('Your Data is Encrypted')
-                flag=1
+            if word == 'TYPE="crypto_LUKS"':
+                x.add_row(["Your Data is Encrypted"])
+                flag = 1
 
-    if flag==0:
-        print('Your Data is not encrypted')
-            
-
-
+    if flag == 0:
+        x.add_row(["Your Data is Encrypted"])
+print(table)
